4443d43314e7dda2da5666168cb3f93d24c6faef
isaacmartinez12345 <imartinez76@student.rccd.edu>
2020-01-20 21:09:52 -0800

    Signed-off-by: isaacmartinez12345 <imartinez76@student.rccd.edu>

---
+++ Hmwk/Display_1.8_Ch1_ Isaac/main.cpp
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Isaac
+ *
+ * Created on January 12, 2020, 3:10 PM
+ */
+
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv) 
+{
+   int numberOfPods, peasPerPod, totalPeas;
+    cout << "Press return after entering a number.\n";
+    cout << "10:\n";
+    cin >> numberOfPods;
+    cout << "9:\n";
+    cin << peasPerPod;
+    totalPeas = numberOfPods * peasPerPod;
+    cout << "If you have ";
+    cout << numberOfPods;
+    cout << "pea pods\n";
+    cout << "and ";
+    cout << peasPerPod;
+    cout << " peas in each pod, then\n";
+    cout << "you have ";
+    cout << totalPeas;
+    cout << " peas in all the pods.\n";
+    return 0;
+    
+}
